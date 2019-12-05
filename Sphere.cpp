6c932d077e90fd9dace2cd4bca153debdbe56d16
jtlemkin <jtlemkin@gmail.com>
2019-12-05 14:30:29 -0800

    added sphere class

---
+++ Sphere.cpp
@@ -0,0 +1,5 @@
+//
+// Created by James Lemkin on 12/5/19.
+//
+
+#include "Sphere.h"
---
+++ Sphere.h
@@ -0,0 +1,15 @@
+//
+// Created by James Lemkin on 12/5/19.
+//
+
+#ifndef HW5_SPHERE_H
+#define HW5_SPHERE_H
+
+#include "Point.h"
+
+class Sphere {
+ private:
+  Point center;
+};
+
+#endif //HW5_SPHERE_H
